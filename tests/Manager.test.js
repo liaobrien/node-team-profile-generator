c38f3be42f54652d4d7d6211822eb2e51bfae933
Lia OBrien <liaobrien123@gmail.com>
2021-10-03 17:14:01 -0400

    manager tests now pass

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,1 +1,20 @@
+const { describe, it, expect } = require("@jest/globals");
 const Manager = require("../lib/Manager");
+
+describe("Manager", () => {
+      describe("getOfficeNum", () => {
+            it("should return the manager's office number", () => {
+                  const newManager = new Manager("John Doe", 1, "jdoe@test.com", 123);
+
+                  expect(newManager.getOfficeNum()).toEqual("Office: 123");
+            })
+      });
+
+      describe("getRole", () => {
+            it("should only return the role 'manager'", () => {
+                  const newManager = new Manager("John Doe", 1, "jdoe@test.com", 123);
+
+                  expect(newManager.getRole()).toEqual("Manager");
+            })
+      });
+})
