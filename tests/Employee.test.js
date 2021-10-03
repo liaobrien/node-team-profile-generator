4dbe13f5e5963bc3ebad3c8c33eb86f7a2c1cc75
Lia OBrien <liaobrien123@gmail.com>
2021-10-03 16:55:00 -0400

    employee.js tests now pass

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,2 +1,34 @@
 const Employee = require("../lib/Employee");
 
+describe("Employee", () => {
+
+      describe("getName", () => {
+            it("should return the name of the employee", () => {
+                  // arrange
+                  const newEmployee = new Employee("John Doe", 1, "jdoe@test.com");
+
+                  // act
+                  expect(newEmployee.getName()).toEqual("Name: John Doe");
+
+                  // assert
+            })
+      });
+
+      describe("getId", () => {
+            it("should return the employee's id number", () => {
+                  const newEmployee = new Employee("John Doe", 1, "jdoe@test.com");
+
+                  expect(newEmployee.getId()).toEqual("ID: 1");
+            })
+
+      });
+
+      describe("getEmail", () => {
+            it("should return the employee's email", () => {
+                  const newEmployee = new Employee("John Doe", 1, "jdoe@test.com");
+
+                  expect(newEmployee.getEmail()).toEqual("Email: jdoe@test.com");
+            })
+      })
+})
+
