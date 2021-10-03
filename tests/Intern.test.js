62739e698cc7713364dea86a2bd6d3c665c7e8dc
Lia OBrien <liaobrien123@gmail.com>
2021-10-03 17:26:40 -0400

    intern tests now pass

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,1 +1,20 @@
+const { describe, expect, it } = require("@jest/globals");
 const Intern = require("../lib/Intern");
+
+describe("Intern", () => {
+      describe("getSchool", () => {
+            it("should return the intern's school", () => {
+                  const newIntern = new Intern("John Doe", 1, "jdoe@test.com", "Penn");
+
+                  expect(newIntern.getSchool()).toEqual("School: Penn");
+            })
+      });
+
+      describe("getRole", () => {
+            it("should return only the role of 'intern'", () => {
+                  const newIntern = new Intern("John Doe", 1, "jdoe@test.com", "Penn");
+
+                  expect(newIntern.getRole()).toEqual("Intern");
+            })
+      })
+})
