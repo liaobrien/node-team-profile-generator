2582bcd974aa4bc2f3a4e2ef99fe587b61d4005a
Lia OBrien <liaobrien123@gmail.com>
2021-10-03 17:21:35 -0400

    engineer tests now pass

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,1 +1,20 @@
+const { describe, it, expect } = require("@jest/globals");
 const Engineer = require("../lib/Engineer");
+
+describe("Engineer", () => {
+      describe("getGithub", () => {
+            it("should return the engineer's github username", () => {
+                  const newEngineer = new Engineer("John Doe", 1, "jdoe@test.com", "jdoe123");
+
+                  expect(newEngineer.getGithub()).toEqual("GitHub: jdoe123");
+            })
+      });
+
+      describe("getRole", () => {
+            it("should only return the role of 'engineer'", () => {
+                  const newEngineer = new Engineer("John Doe", 1, "jdoe@test.com", "jdoe123");
+
+                  expect(newEngineer.getRole()).toEqual("Engineer");
+            })
+      })
+})
