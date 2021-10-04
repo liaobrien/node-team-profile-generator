1ed794a688535471f9bbc02b08e6580d70687025
Lia OBrien <liaobrien123@gmail.com>
2021-10-03 20:18:33 -0400

    edited html template, created all questions

--- index.js
+++ index.js
@@ -5,3 +5,78 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const jest = require("jest");
+
+const managerQuestions = [
+      {
+            type: "input",
+            message: "What is the manager's name?",
+            name: "name",
+      },
+      {
+            type: "input",
+            message: "What is the manager's ID number?",
+            name: "id",
+      },
+      {
+            type: "input",
+            message: "What is their email address?",
+            name: "email",
+      },
+      {
+            type: "input",
+            message: "What is their office number?",
+            name: "office"
+      }
+]
+
+const engineerQuestions = [
+      {
+            type: "input",
+            message: "What is the engineer's name?",
+            name: "name",
+      },
+      {
+            type: "input",
+            message: "What is the engineer's ID number?",
+            name: "id",
+      },
+      {
+            type: "input",
+            message: "What is the engineer's address?",
+            name: "email",
+      },
+      {
+            type: "input",
+            message: "What is the engineer's GitHub?",
+            name: "github"
+      }
+]
+
+const internQuestions = [
+      {
+            type: "input",
+            message: "What is the intern's name?",
+            name: "name",
+      },
+      {
+            type: "input",
+            message: "What is the intern's ID number?",
+            name: "id",
+      },
+      {
+            type: "input",
+            message: "What is the intern's address?",
+            name: "email",
+      },
+      {
+            type: "input",
+            message: "What is the intern's school?",
+            name: "school"
+      }
+]
+
+const teamAdd = {
+      type: "list",
+      message: "What type of team member would you like to add?",
+      choices: ["Engineer", "Intern", "None"]
+}
