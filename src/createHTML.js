2e4d1fd8bf1e6f813a9a972f2a5839a8067250f2
Lia OBrien <liaobrien123@gmail.com>
2021-10-04 20:07:34 -0400

    linked createHTML to my index, and now an html file gets created successfully

--- src/createHTML.js
+++ src/createHTML.js
@@ -48,3 +48,69 @@ const createIntern = (intern) => {
         </ul>
       </div>`;
 }
+
+function generateHTML(data) {
+      cardsArr = [];
+
+      for (let i = 0; i < data.length; i++) {
+            const employee = data[i];
+            const role = employee.getRole();
+
+            if (role === 'Manager') {
+                  const managerCard = createManager(employee);
+
+                  cardsArr.push(managerCard);
+            }
+
+            if (role === 'Engineer') {
+                  const engineerCard = createEngineer(employee);
+
+                  cardsArr.push(engineerCard);
+            }
+
+            if (role === 'Intern') {
+                  const internCard = createIntern(employee);
+
+                  cardsArr.push(internCard);
+            }
+      }
+
+      const teamCards = cardsArr.join('');
+
+      const createTeam = createHTMLPage(teamCards)
+      return createTeam;
+}
+
+module.exports = generateHTML;
+
+
+
+function createHTMLPage(teamCards) {
+      return `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>My Team</title>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid bg-danger text-white">
+      <div class="container">
+        <h1 class="display-4 text-center">My Team</h1>
+      </div>
+    </div>
+    <div class="row d-flex justify-content-center">
+      ${teamCards}
+    </div>
+  </body>
+</html>`;
+}
